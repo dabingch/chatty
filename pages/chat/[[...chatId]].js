447884dad3d0428e8be8f4294a264aed7ece87b0
Hao Chen <dabingchhh@gmail.com>
2023-07-12 13:57:56 +0800

    Added auth0

---
+++ pages/chat/[[...chatId]].js
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Chat() {
+  return (
+    <div>
+      <Head>
+        <title>Next JS ChatGPT Starter</title>
+      </Head>
+      <h1>Welcome to the chat page</h1>
+    </div>
+  );
+}
