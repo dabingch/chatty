b2acd88136b980f9c473a38c9933cccf1eda8b51
Hao Chen <dabingchhh@gmail.com>
2023-07-12 14:50:59 +0800

    Added redirect after signin

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,61 @@
 import Head from "next/head";
 import Link from "next/link";
 
+import { useUser } from "@auth0/nextjs-auth0/client";
+import { getSession } from "@auth0/nextjs-auth0";
+
+export const getServerSideProps = async (ctx) => {
+  const { user } = await getSession(ctx.req, ctx.res);
+
+  if (!user) {
+    return {
+      props: {},
+    };
+  }
+
+  return {
+    redirect: {
+      destination: "/chat",
+    },
+  };
+};
+
 export default function Home() {
+  const { isLoading, error, user } = useUser();
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>{error.message}</div>;
+  }
+
   return (
     <div>
       <Head>
-        <title>Next JS ChatGPT Starter</title>
+        <title>Chatty Pete - Login or Signup</title>
       </Head>
-      <h1>Welcome to the Next JS &amp; ChatGPT Starter</h1>
-      <div>
-        <Link href="/api/auth/login">Login</Link>
+      <div className="flex min-h-screen w-full items-center justify-center bg-gray-800 text-center text-white">
+        <div>
+          {!user && (
+            <>
+              <Link
+                href="/api/auth/login"
+                className="rounded-md bg-emerald-500 px-4 py-2 text-white hover:bg-emerald-600"
+              >
+                Login
+              </Link>
+              <Link
+                href="/api/auth/signup"
+                className="rounded-md bg-emerald-500 px-4 py-2 text-white hover:bg-emerald-600"
+              >
+                Signup
+              </Link>
+            </>
+          )}
+        </div>
+        <Link href="/api/auth/logout">Logout</Link>
       </div>
     </div>
   );
