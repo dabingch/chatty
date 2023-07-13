4a546d40df434a2a8b52a573ea00ce35cce84ae0
Hao Chen <dabingchhh@gmail.com>
2023-07-13 16:50:54 +0800

    Added get chat list endpoint

--- components/ChatSidebar.js
+++ components/ChatSidebar.js
@@ -1,9 +1,43 @@
+import { useEffect, useState } from "react";
 import Link from "next/link";
 
 const ChatSidebar = () => {
+  const [chatList, setChatList] = useState([]);
+
+  useEffect(() => {
+    const loadChatList = async () => {
+      const response = await fetch("/api/chat/getChatList", {
+        method: "GET",
+      });
+
+      const data = await response.json();
+      console.log("GET CHAT LIST", data);
+
+      setChatList(data?.chats || []);
+    };
+
+    loadChatList();
+  }, []);
+
   return (
-    <div className="bg-gray-900 text-white">
-      <Link href="/api/auth/logout">Logout</Link>
+    <div className="flex flex-col overflow-hidden bg-gray-900 text-white">
+      <Link className="side-menu-item" href="/chat">
+        New Chat
+      </Link>
+      <div className="flex-1 overflow-auto bg-gray-950">
+        {chatList.map((chat) => (
+          <Link
+            className="side-menu-item"
+            key={chat._id}
+            href={`/chat/${chat._id}`}
+          >
+            {chat.title}
+          </Link>
+        ))}
+      </div>
+      <Link className="side-menu-item" href="/api/auth/logout">
+        Logout
+      </Link>
     </div>
   );
 };
---
+++ pages/api/chat/getChatList.js
@@ -0,0 +1,27 @@
+import { getSession } from "@auth0/nextjs-auth0";
+import clientPromise from "lib/mongodb";
+
+export default async function handler(req, res) {
+  try {
+    const { user } = await getSession(req, res);
+    const client = await clientPromise;
+    const db = await client.db("chatty");
+    const chats = await db
+      .collection("chats")
+      .find(
+        { userId: user.sub },
+        {
+          projection: {
+            userId: 0,
+            messages: 0,
+          },
+        }
+      )
+      .sort({ _id: -1 })
+      .toArray();
+
+    res.status(200).json({ chats });
+  } catch (error) {
+    res.status(500).json({ message: "Error getting chat" });
+  }
+}
